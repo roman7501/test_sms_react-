a8a9be6feaf8f4b25351ef534497eadad8d92513
Roman <romanjeanelie@yahoo.fr>
2020-08-11 10:09:42 +0200

    correction function

--- functions/send-sms.js
+++ functions/send-sms.js
@@ -1,30 +1,29 @@
 require("dotenv").config();
-export function handler(event, context, callback) {
-  const { ACCOUNT_SID, AUTH_TOKEN } = process.env;
-  const twilio = require("twilio");
-  const client = new twilio(ACCOUNT_SID, AUTH_TOKEN);
 
-  exports.handler = function (event, context, callback) {
-    client.messages
-      .create({
-        body: "Is this working ??",
-        to: "+33661396623", // Text this number
-        from: "+14042366753", // From a valid Twilio number
+const { ACCOUNT_SID, AUTH_TOKEN } = process.env;
+const twilio = require("twilio");
+const client = new twilio(ACCOUNT_SID, AUTH_TOKEN);
+
+exports.handler = function (event, context, callback) {
+  client.messages
+    .create({
+      body: "Is this working ??",
+      to: "+33661396623", // Text this number
+      from: "+14042366753", // From a valid Twilio number
+    })
+    .then(() =>
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers":
+            "Origin, X-Requested-With, Content-Type, Accept",
+        },
+        body: "Created",
       })
-      .then(() =>
-        callback(null, {
-          statusCode: 200,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers":
-              "Origin, X-Requested-With, Content-Type, Accept",
-          },
-          body: "Created",
-        })
-      )
-      .catch((e) => {
-        console.log("heelo");
-        callback(e);
-      });
-  };
-}
+    )
+    .catch((e) => {
+      console.log("heelo");
+      callback(e);
+    });
+};
