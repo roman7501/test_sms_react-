07c0511c2b0a2365f56e84d6626beb5b7fbfc4e7
Roman <romanjeanelie@yahoo.fr>
2020-08-11 09:26:12 +0200

    change scripts

--- src/Components/SendSMS.js
+++ src/Components/SendSMS.js
@@ -1,41 +1,18 @@
 import React from "react";
 
 const SendSMS = () => {
-  const handleSend = async (event) => {
-    event.preventDefault();
-    console.log("clicked !");
-    try {
-      const response = await fetch("/", {
-        method: "post",
-        headers: {
-          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        },
-        // parse and submit all included form data
-        body: "sms sent",
-      });
-
-      // if it was successful show success message
-      if (response.status === 200) {
-        console.log("it works");
-      } else {
-        console.log("it doesnt work");
-      }
-    } catch (e) {
-      console.error(e);
-    }
+  const handleSend = () => {
+    console.log("submit!");
+    fetch("http://localhost:9000/send-sms", {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    }).then(console.log("sms sent !"));
   };
   return (
     <div>
-      <form
-        name="contact"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSend}
-      >
-        <button type="submit">Snd SMS!</button>
-      </form>
+      <button onClick={() => handleSend()}>Send SMS!</button>
     </div>
   );
 };
