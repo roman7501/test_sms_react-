b73f64ea39d02b8568faf9d9ec706e0564f4c98e
Roman <romanjeanelie@yahoo.fr>
2020-08-11 09:57:14 +0200

    correction function sms

--- functions/send-sms.js
+++ functions/send-sms.js
@@ -5,13 +5,12 @@ export function handler(event, context, callback) {
   const client = new twilio(ACCOUNT_SID, AUTH_TOKEN);
 
   exports.handler = function (event, context, callback) {
-    Promise(
-      client.messages.create({
+    client.messages
+      .create({
         body: "Is this working ??",
         to: "+33661396623", // Text this number
         from: "+14042366753", // From a valid Twilio number
       })
-    )
       .then(() =>
         callback(null, {
           statusCode: 200,
--- src/Components/SendSMS.js
+++ src/Components/SendSMS.js
@@ -4,7 +4,7 @@ const SendSMS = () => {
   const handleSend = () => {
     console.log("submit!");
     fetch("/.netlify/functions/send-sms", {
-      method: "POST",
+      method: "GET",
       headers: {
         "Content-Type": "text/plain",
       },
